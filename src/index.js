42d7da6c3c04a1e94bfcfe64c76b1a934cb05ff4
Leigh Dotey <leighdotey@gmail.com>
2023-12-28 14:18:36 -0500

    completasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    buildTask(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+function buildTask(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X '
+  p.textContent = `${todo}  `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+//listen for a submit event on the <form> element
+//invoke preventDefault() method on the submit event when our event listener "sees" the event
+//see the task string that I provided appear in the DOM after the submit button has been activated
